7052f74c30b8d485b385cee4a67f18bb8c9276ef
cgpgh <cyril73@hotmail.com>
2020-05-27 20:42:33 -0700

    Update example1.tf

--- create-aws-instance/example1.tf
+++ create-aws-instance/example1.tf
@@ -1,13 +1,14 @@
 provider "aws" {
   profile    = "default"
-  region     = "us-east-1"
+  region     = var.region
   shared_credentials_file = "$HOME/.aws/credentials"
-  
+
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
-  # Resource tags
-  Name       = "${var.ec2name}"
+  tags = {
+  Name = "PTL1"
+  }
 }
